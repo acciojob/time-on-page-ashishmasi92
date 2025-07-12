dd43d8dda481e61729a180f76db2fb0e7f2736be
ashish <ashishmasih92g@gmail.com>
2025-07-13 00:01:10 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+let [count,setCount] = useState(0)
+
+
+useEffect(()=>{
+
+ let id=  setInterval(()=>{
+    setCount((prev)=>{return prev+1})
+  },1000)
+
+return ()=>clearInterval(id)
+
+},[])
+
   return (
     <div>
+
+      you have been on this page for {count} second.
         {/* Do not remove the main div */}
     </div>
   )
